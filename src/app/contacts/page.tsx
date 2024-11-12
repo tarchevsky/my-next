37e2901bbe23d4a2b6cac5d37d9a164074bb24c0
tarchevsky <i.tarchevsky@gmai.com>
2024-11-13 01:52:13 +0300

    Добавление констант сайта и константы - имя сайта, плюс подключение в layout meta с этой переменной и добавление на другие страницы

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,4 +1,9 @@
 import Carousel from "@/components/carousel/Carousel";
+import type {Metadata} from "next";
+
+export const metadata: Metadata = {
+	title: 'Контакты',
+}
 
 export default function ContactsPage() {
 	return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
 import Image from 'next/image'
+import type {Metadata} from "next";
+
+export const metadata: Metadata = {
+	title: 'Главная',
+}
 
 export default function Home() {
 	return (
