ed65455ac9eeab5b8d99a21068abb87eb5ebd164
tarchevsky <i.tarchevsky@gmai.com>
2024-11-13 01:52:55 +0300

    Подключение доп формы в разделе контакты

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,14 +1,22 @@
 import Carousel from "@/components/carousel/Carousel";
 import type {Metadata} from "next";
+import ContactFormPlus from "@/components/contactFormPlus/ContactFormPlus";
+import {deliveryForm} from "@/data/delivery-form";
 
 export const metadata: Metadata = {
 	title: 'Контакты',
 }
 
 export default function ContactsPage() {
+	const serializedFields = JSON.parse(JSON.stringify(deliveryForm))
 	return (
 		<div className='cont'>
 			<Carousel />
+			<ContactFormPlus
+				title="Контактная форма"
+				fields={serializedFields}
+				storageKey="myCustomFormData"
+			/>
 		</div>
 	)
 }
