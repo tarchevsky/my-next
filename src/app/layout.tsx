cce4c46badf8c189df277592517e4576febc1a8a
tarchevsky <i.tarchevsky@gmai.com>
2024-11-24 18:14:23 +0300

    Компонент Metrika с добавлением в layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,35 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import Header from "@/components/header/Header";
-import Footer from "@/components/footer/Footer";
-import ScrollToTop from "@/components/scrollToTop/ScrollToTop";
-import {SITE_NAME} from "@/constants/site.constants";
-import {ReactNode} from "react";
+import type { Metadata } from 'next'
+import './globals.css'
+import Header from '@/components/header/Header'
+import Footer from '@/components/footer/Footer'
+import ScrollToTop from '@/components/scrollToTop/ScrollToTop'
+import { SITE_NAME } from '@/constants/site.constants'
+import { ReactNode } from 'react'
+import Metrika from '@/components/metrika/Metrika'
+
+const yId = process.env.NEXT_PUBLIC_YID // id яндекс метрики
 
 export const metadata: Metadata = {
-    title: {
-        default: SITE_NAME,
-        template: `%s | ${SITE_NAME}`,
-    },
+	title: {
+		default: SITE_NAME,
+		template: `%s | ${SITE_NAME}`
+	}
 }
 
 export default function RootLayout({
-  children,
+	children
 }: Readonly<{
-  children: ReactNode;
+	children: ReactNode
 }>) {
-  return (
-    <html lang="ru">
-      <body>
-        <Header />
-        {children}
-        <ScrollToTop />
-        <Footer />
-      </body>
-    </html>
-  );
+	return (
+		<html lang='ru'>
+			<body>
+				<Header />
+				{children}
+				<ScrollToTop />
+				<Footer />
+				{yId ? <Metrika yId={yId} /> : null}
+			</body>
+		</html>
+	)
 }
