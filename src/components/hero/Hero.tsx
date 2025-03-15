b2e64c8fa8c0ecc5d081594943344089e1797b4c
tarchevskyivan <i.tarchevsky@gmail.com>
2025-03-15 20:11:25 +0300

    Поменял в Hero img на Image и добавил priority
    вместо lodaing='lasy' и сделал условие - если alt нет, ставить дефолтный alt

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,20 +1,23 @@
 import { HeroProps } from '@/types'
+import Image from 'next/image'
 
 const Hero = ({ title, buttonText, src, alt }: HeroProps) => {
 	return (
 		<main className='hero md:min-h-[80vh]'>
 			<div className='relative hero-content flex-col lg:flex-row'>
-				<img
+				<Image
 					className='lg:w-1/2 h-[600px] object-cover rounded-box shadow-2xl'
 					src={src}
-					alt={alt}
-					loading='lazy'
+					alt={alt || 'Картинка без описания'}
+					priority
+					width={600}
+					height={600}
 				/>
 				<div className='absolute lg:relative lg:ml-[-100px] z-10'>
 					<h1
 						className='md:text-6xl font-bold'
 						dangerouslySetInnerHTML={{
-							__html: title
+							__html: title,
 						}}
 					/>
 					<button className='btn btn-primary btn-lg'>{buttonText}</button>
